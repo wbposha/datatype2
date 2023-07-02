e0fa307e6f6c11917fa39a1469c8928f7726048f
ywb <ywbposha@gmail.com>
2023-07-03 01:57:53 +0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        int Score = 100;
+        int replenishment = 1100;
 
-        // Объявляете переменные для входных данных и
-        // параметров программы: начального счёта,
-        // суммы пополнения и тп
+        int bonus;
+        if (replenishment > 1000) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;}
 
-        // Условным оператором проверяете, превысила ли
-        // сумма пополнения порог, и для этих двух разных
-        // сценариев рассчитываете сумму бонуса и выводите
-        // на экран.
+        System.out.println(Score + replenishment + bonus);
     }
 }
